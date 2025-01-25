558ccb7bd7aac164a8a4abddc9c7fa3f8253189d
Saul Renderfrance <srenderfrance@gmail.com>
2025-01-25 22:17:06 +0100

    second commit

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ pygame.init()
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
