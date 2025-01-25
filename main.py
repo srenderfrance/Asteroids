48ee7b272d5ec56afdc65f0cf5eafa2de644274d
Saul Renderfrance <srenderfrance@gmail.com>
2025-01-25 22:09:52 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.display.flip()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
